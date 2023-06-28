4e7dbde4968e583b22b6db6a40ff8cfd090bd10b
DylanD <dylan.doherty35@gmail.com>
2023-06-28 10:00:26 +0100

    LATEST

--- mongo/cadLogIn.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const Schema = mongoose.Schema({
-    ip: String,
-    userId: String,
-    loggedIn: Boolean
-})
-
-module.exports = mongoose.model('CAD Log In Info', Schema)
